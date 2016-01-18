3547b156933c18d5d5a65e66a483648531d1fddc
Arjun Komath <arjunkomath@gmail.com>
2016-01-19 01:33:19 +0530

    beta ui

--- App/Pages/InfoPage.js
+++ App/Pages/InfoPage.js
@@ -7,6 +7,7 @@ var {
     Text,
     View,
     ScrollView,
+    Image,
 } = React;
 
 var UserWidget = require('../UserWidget');
@@ -23,7 +24,28 @@ var InfoPage = React.createClass({
         return (
             <ScrollView
             style={styles.container}>
+
+            <Text style={styles.head}>MAKERS</Text>
+            <View style={styles.users}>
+            {this.state.post.makers.map( (maker, i) => <Image key={i} source={{uri: maker.image_url['50px'] }} style={styles.thumbnail} /> ) }
+            </View>
+
+            <Text style={styles.head}>UPVOTES</Text>
+            <View style={styles.users}>
+            {this.state.post.votes.map( (vote, i) => <Image key={i} source={{uri: vote.user.image_url['50px'] }} style={styles.thumbnail} /> ) }
+            </View>
+
+            <Text style={styles.head}>PLATFORMS</Text>
+            <View style={styles.users}>
+            {this.state.post.platforms.map( (platform, i) => <Text key={i} style={styles.tag}>{platform.name}</Text> ) }
+            </View>
+
+            <Text style={styles.head}>CATEGORY</Text>
+            <Text style={styles.tag}>{this.state.post.category_id}</Text>
+
+            <Text style={styles.head}>HUNTED BY</Text>
             <UserWidget user={this.state.post.user} />
+
             </ScrollView>
         );
     },
@@ -34,6 +56,30 @@ var styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    head: {
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginBottom: 5,
+        marginLeft: 10,
+        color: '#000000'
+    },
+    tag: {
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 10,
+        color: '#3e3e3e'
+    },
+    thumbnail: {
+        height: 50,
+        width: 50,
+        borderRadius: 50,
+        marginLeft: 5
+    },
+    users: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: 5
+    }
 });
 
 module.exports = InfoPage;
