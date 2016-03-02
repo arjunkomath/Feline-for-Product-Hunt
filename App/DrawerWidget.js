34148ee1269692b7915e12605aef7ef67d945523
Arjun <arjun@Arjuns-MacBook-Pro.local>
2016-03-02 22:09:05 +0530

    update codepush

--- App/DrawerWidget.js
+++ App/DrawerWidget.js
@@ -107,7 +107,7 @@ var Drawer = React.createClass({
             Contact
             </Icon.Button>
 
-            <Text style={styles.version}>Version 1.0.1</Text>
+            <Text style={styles.version}>Version 1.0.2</Text>
 
             </ScrollView>
 
